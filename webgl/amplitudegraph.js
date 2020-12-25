b6eee36ceb337730616a76a384df76b2cdfe6d9b
skylarkgit <abhay199658@gmail.com>
2020-12-25 09:54:21 +0530

    [fix] amplitude logic

--- webgl/amplitudegraph.js
+++ webgl/amplitudegraph.js
@@ -7,13 +7,13 @@ navigator.mediaDevices.getUserMedia({ audio: true }).then(
   (error) => {}
 );
 
-
 function soundGraph(stream) {
   audioContext = new AudioContext();
   const mediaStreamSource = audioContext.createMediaStreamSource(stream);
   analyzer = audioContext.createAnalyser();
-  analyzer.smoothingTimeConstant = 0.1;
+  analyzer.smoothingTimeConstant = 0;
   mediaStreamSource.connect(analyzer); // script processor is used as an intermediary between analyzer
+  analyzer.fftSize = 32;
 
   const canvas = document.getElementById("liveamp");
   const devicePixelRatio = window.devicePixelRatio || 1;
@@ -54,9 +54,24 @@ function soundGraph(stream) {
   }
 
   setInterval(() => {
-    const dataArray = new Uint8Array(this.analyzer.frequencyBinCount);
-    this.analyzer.getByteFrequencyData(dataArray);
-    const amp = Math.max.apply(null, dataArray);
-    dataHandler.push(amp/255);
+    const dataArray = new Uint8Array(analyzer.frequencyBinCount);
+    analyzer.getByteFrequencyData(dataArray);
+    const amp = getVolume(dataArray, analyzer.frequencyBinCount);
+    dataHandler.push(amp / 500);
+    console.log(amp)
   }, 20);
+
+  // https://stackoverflow.com/questions/62083981/analysernode-getfloatfrequencydata-returns-negative-values
+  function getVolume(dataArray, binCount) {
+    let bufferLength = binCount;
+    let totalAntilogAmplitude = 0;
+    for (let i = 0; i < bufferLength; i++) {
+      let thisAmp = dataArray[i]; // amplitude of current bin
+      let thisAmpAntilog = Math.pow(10, thisAmp / 10); // antilog amplitude for adding
+      totalAntilogAmplitude = totalAntilogAmplitude + thisAmpAntilog;
+    }
+    console.log(totalAntilogAmplitude, binCount)
+    let amplitude = 10 * Math.log10(totalAntilogAmplitude);
+    return amplitude;
+  }
 }
